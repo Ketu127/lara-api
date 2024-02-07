384c7023eaa182893d2573dfd90e1766aab477dd
ketvek <ketanvekariya947@gmail.com>
2024-02-07 11:57:18 +0000

    API controller Mathods settings

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -7,5 +7,28 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    //
+    //Register API (Post request)
+    public function register(Request $request){
+
+    }
+
+    //Login API (Post request)
+    public function login(Request $request){
+
+    }
+
+    //profile API (Get request)
+    public function profile(Request $request){
+
+    }
+
+    //Refresh Token API (Get Request)
+    public function refresh|(){
+
+    }
+
+    //Logout API(Get)
+    public function logout(){
+        
+    }
 }
